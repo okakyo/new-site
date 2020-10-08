ac5cb145cecbd61df9a93c83cff101f0e3969afe
=okakyo <=0622okakyo@gmail.com>
2020-10-08 21:32:47 +0900

    フォーム画面を追加

--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -1,6 +1,19 @@
 <template>
   <index-grid>
-    <index-card page-title="部活の紹介"> DENX について </index-card>
+    <index-card page-title="部活の紹介">
+      <v-layout>
+        <v-row justify="center">
+          <v-col cols="8">
+            <v-form class="my-7">
+              <v-text-field outlined name="title" label="件名" />
+              <v-text-field outlined name="email" label="メールアドレス" />
+              <v-textarea outlined name="message" label="お問い合わせ内容" />
+              <v-btn block outlined>送信</v-btn>
+            </v-form>
+          </v-col>
+        </v-row>
+      </v-layout>
+    </index-card>
   </index-grid>
 </template>
 
