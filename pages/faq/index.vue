c95fd5642906e86f6457deda6688497605a3fb24
=okakyo <=0622okakyo@gmail.com>
2020-10-11 20:49:40 +0900

    仕様をどんどん変更

--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -1,13 +1,23 @@
 <template>
   <index-grid>
-    <index-card page-title="部活の紹介">
+    <index-card page-title="お問い合わせ">
       <v-layout>
         <v-row justify="center">
           <v-col cols="8">
             <v-form class="my-7">
-              <v-text-field outlined name="title" label="件名" />
-              <v-text-field outlined name="email" label="メールアドレス" />
-              <v-textarea outlined name="message" label="お問い合わせ内容" />
+              <v-text-field outlined name="title" type="input" label="件名" />
+              <v-text-field
+                outlined
+                name="email"
+                type="email"
+                label="メールアドレス"
+              />
+              <v-textarea
+                outlined
+                name="message"
+                type="text"
+                label="お問い合わせ内容"
+              />
               <v-btn block outlined>送信</v-btn>
             </v-form>
           </v-col>
