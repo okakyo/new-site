70c21095089e6cda4f0446de8ab35022987605b5
=okakyo <=0622okakyo@gmail.com>
2020-10-11 16:33:41 +0900

    ポートフォリオサイトに変更

---
+++ pages/blog/tag/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <div />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'IntroductionPages',
+  components: {},
+})
+</script>
