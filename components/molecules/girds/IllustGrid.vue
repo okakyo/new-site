6ca584fd63fe40ab2966f83a38a0b926856febf1
=okakyo <=0622okakyo@gmail.com>
2020-10-11 17:26:58 +0900

    記事を編集

--- components/molecules/girds/IllustGrid.vue
+++ components/molecules/girds/IllustGrid.vue
@@ -5,7 +5,7 @@
         <v-col cols="8" md="5" class="mb-5">
           <slot name="LeftSide" />
         </v-col>
-        <v-col cols="12" md="4">
+        <v-col cols="6" md="4">
           <slot name="RightSide" />
         </v-col>
       </v-row>
