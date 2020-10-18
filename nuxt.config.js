230b39b1311a659e06e8ab5751bc8ad990adf634
=okakyo <=0622okakyo@gmail.com>
2020-10-18 17:57:15 +0900

    \環境を再構築

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s',
-    title: 'DENX Official Page',
+    title: 'Blog Page',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
