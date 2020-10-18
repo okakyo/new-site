0235395319bec376eccecedf0dd9e0a9b33b0d19
=okakyo <=0622okakyo@gmail.com>
2020-10-18 18:16:29 +0900

    動的ページに対応中

--- nuxt.config.js
+++ nuxt.config.js
@@ -78,7 +78,6 @@ export default {
     async routes() {
       const { $content } = require('@nuxt/content')
       const files = await $content().only(['path']).fetch()
-
       return files.map((file) => (file.path === '/index' ? '/' : file.path))
     },
     fallback: true,
