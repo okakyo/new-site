43ceb411451bc6efef9ba442b6b1830470a4e596
=okakyo <=0622okakyo@gmail.com>
2020-10-21 18:40:56 +0900

    サイドバーのリファクタリング

--- components/molecules/girds/ArticleGrid.vue
+++ components/molecules/girds/ArticleGrid.vue
@@ -1,36 +1,25 @@
 <template>
   <v-layout class="mt-2">
     <v-row wrap justify="center">
-      <v-col cols="12" md="10" lg="9">
+      <v-col cols="12" md="9" lg="8">
         <slot name="LeftSide" />
       </v-col>
-
-      <v-navigation-drawer clipped app translate>
-        <v-list dense flat>
-          <v-list-item
-            v-for="(item, i) in toc"
-            :key="i"
-            nuxt
-            color="info"
-            link
-            :to="'/blog/' + pageSlug + '#' + item.id"
-          >
-            <v-list-item-title v-if="item.depth == 2" class="text-primary">{{
-              item.text
-            }}</v-list-item-title>
-            <v-list-item-subtitle v-else-if="item.depth == 3" class="pl-3">{{
-              item.text
-            }}</v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <v-col lg="3">
+        <slot name="RightSide" />
+      </v-col>
     </v-row>
+    <sidebar-toc :toc="toc" :page-slug="pageSlug" />
   </v-layout>
 </template>
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+import SidebarToc from '@/components/molecules/lists/SidebarToc.vue'
+
 export default defineComponent({
-  name: 'IllustGrid',
+  name: 'ArticleGrid',
+  components: {
+    SidebarToc,
+  },
   props: {
     toc: Array,
     pageSlug: String,
--- components/molecules/lists/SidebarToc.vue
+++ components/molecules/lists/SidebarToc.vue
@@ -1,10 +1,33 @@
 <template>
-  <div />
+  <v-navigation-drawer clipped app translate>
+    <v-list dense flat>
+      <v-list-item
+        v-for="(item, i) in toc"
+        :key="i"
+        nuxt
+        color="info"
+        link
+        :to="'/blog/' + pageSlug + '#' + item.id"
+      >
+        <v-list-item-title v-if="item.depth == 2" class="text-primary">{{
+          item.text
+        }}</v-list-item-title>
+        <v-list-item-subtitle v-else-if="item.depth == 3" class="pl-3">{{
+          item.text
+        }}</v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
+
 export default defineComponent({
-  name: 'NavList',
+  name: 'SidebarToc',
+  props: {
+    toc: Array,
+    pageSlug: String,
+  },
 })
 </script>
