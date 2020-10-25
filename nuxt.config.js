fbbf1282d4ef98692f9aa65dfd164e3ccfeb65a3
=okakyo <=0622okakyo@gmail.com>
2020-10-25 15:07:56 +0900

    OGP動作確認

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#',
+    },
     titleTemplate: '%s',
     title: 'Blog Page',
     meta: [
@@ -39,7 +42,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'https://okakyo-new-portfolio.netlify.app/img/ogp.png',
+        content: '/img/ogp.png',
       },
       {
         hid: 'twitter:card',
