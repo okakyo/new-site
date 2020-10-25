b5d48a429434a607f06f36a8c84868b6f31c57af
=okakyo <=0622okakyo@gmail.com>
2020-10-26 00:51:32 +0900

    CSS まったくよくわからん

--- pages/blog/article/_id.vue
+++ pages/blog/article/_id.vue
@@ -30,8 +30,6 @@ export default {
   head() {
     return {
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
           hid: 'description',
           name: 'description',
--- pages/index.vue
+++ pages/index.vue
@@ -21,5 +21,48 @@ export default defineComponent({
       waveImg,
     }
   },
+  head() {
+    return {
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: "Okakyo's Blog Site",
+        },
+        {
+          hid: 'og:site_name',
+          property: 'og:site_name',
+          content: "Okakyo's Blog",
+        },
+        { hid: 'og:type', property: 'og:type', content: 'website' },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://okakyo-new-portfolio.netlify.app',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: "Okakyo's Blog Site",
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: "Okakyo's Blog Site",
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://okakyo-new-portfolio.netlify.app/img/ogp.png',
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        { hid: 'twitter:site', name: 'twitter:site', content: '@31415O_Kyo' },
+      ],
+    }
+  },
 })
 </script>
