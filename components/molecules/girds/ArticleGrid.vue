fcf326360d886e56994fd6128066893e796f2edc
=okakyo <=0622okakyo@gmail.com>
2020-11-02 07:39:40 +0900

    レスポンシブに幅が変化しなかったバグを修正

--- components/molecules/girds/ArticleGrid.vue
+++ components/molecules/girds/ArticleGrid.vue
@@ -1,15 +1,13 @@
 <template>
-  <v-layout class="mt-2">
-    <v-row justify="center">
-      <v-col cols="12" md="9" lg="8">
-        <slot name="LeftSide" />
-      </v-col>
-      <v-col lg="3">
-        <slot name="RightSide" />
-      </v-col>
-    </v-row>
+  <v-row justify="center">
     <sidebar-toc :toc="toc" :page-slug="pageSlug" />
-  </v-layout>
+    <v-col cols="12" md="9" lg="8">
+      <slot name="LeftSide" />
+    </v-col>
+    <v-col lg="3">
+      <slot name="RightSide" />
+    </v-col>
+  </v-row>
 </template>
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
