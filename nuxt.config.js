f2e57c972f38c09f774da3b73349cf91c2e4c2eb
Kyohei Oka <0622okakyo@gmail.com>
2021-02-12 20:57:57 +0900

    画像追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
       prefix: 'og: http://ogp.me/ns#',
     },
     titleTemplate: '%s',
-    title: 'Blog Page',
+    title: "Okakyo's Portfolio Site",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
